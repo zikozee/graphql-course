fdeeb393160d6fe06ed0404a7c41ac6e1f93c893
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2024-10-02 09:09:54 +0100

    first commit

---
+++ src/main/java/com/zee/graphqlcourse/GraphqlCourseApplication.java
@@ -0,0 +1,13 @@
+package com.zee.graphqlcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlCourseApplication.class, args);
+    }
+
+}
