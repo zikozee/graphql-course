f8b8056209b7ea2c52bd908508ab4f7e124b2ff0
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2024-10-27 01:53:25 +0100

    Headers With Arguments closes #22

--- src/main/java/com/zee/graphqlcourse/api/CommonController.java
+++ src/main/java/com/zee/graphqlcourse/api/CommonController.java
@@ -1,10 +1,13 @@
 package com.zee.graphqlcourse.api;
 
 import com.zee.graphqlcourse.codegen.DgsConstants;
+import com.zee.graphqlcourse.codegen.types.EmployeeDto;
+import com.zee.graphqlcourse.codegen.types.OutsourcedDto;
 import com.zee.graphqlcourse.codegen.types.PersonAndEntitySearch;
 import com.zee.graphqlcourse.service.CommonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.ContextValue;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
@@ -23,9 +26,48 @@ public class CommonController {
 
     @SchemaMapping(
             typeName = DgsConstants.QUERY_TYPE,
-            field = "employeeWithCompanySearch"
+            field = DgsConstants.QUERY.EmployeeWithCompanySearch
     )
     public List<PersonAndEntitySearch> employeeWithCompanySearch(@Argument("employeeId")String id){
         return commonService.employeeWithCompanySearch(id);
     }
+
+    @SchemaMapping(
+            typeName = DgsConstants.QUERY_TYPE,
+            field = DgsConstants.QUERY.FetchEmployeesUsingHeaders
+    )
+    public List<EmployeeDto> fetchEmployeesUsingHeaders(
+            @ContextValue(name = "header1", required = false) String header1,
+            @ContextValue(value = "header2") String header2,
+            @ContextValue(name = "header3", required = false) String header3
+    ){
+
+        return commonService.fetchEmployeesUsingHeaders(header1, header2, header3);
+    }
+
+    @SchemaMapping(
+            typeName = DgsConstants.QUERY_TYPE,
+            field = DgsConstants.QUERY.FetchOutsourcedUsingHeaders
+    )
+    public List<OutsourcedDto> fetchOutsourcedUsingHeaders(
+            @ContextValue(name = "header4", required = false) String header4,
+            @ContextValue(value = "header5") String header5
+    ){
+
+        return commonService.fetchOutsourcedUsingHeaders(header4, header5);
+    }
+
+    @SchemaMapping(
+            typeName = DgsConstants.QUERY_TYPE,
+            field = DgsConstants.QUERY.FetchEmployeesUsingHeadersAndArgument
+    )
+    public List<EmployeeDto> fetchEmployeesUsingHeadersAndArgument(
+            @ContextValue(name="header6", required = false) String header6,
+            @Argument List<String> staffIds
+    ){
+
+        // note you can use in combination with any query or mutation
+        return commonService.fetchEmployeesUsingHeadersAndArgument(header6, staffIds);
+    }
+
 }
---
+++ src/main/java/com/zee/graphqlcourse/config/RequestHeaderInterceptor.java
@@ -0,0 +1,46 @@
+package com.zee.graphqlcourse.config;
+
+import org.springframework.graphql.server.WebGraphQlInterceptor;
+import org.springframework.graphql.server.WebGraphQlRequest;
+import org.springframework.graphql.server.WebGraphQlResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : Ezekiel Eromosei
+ * @code @created : 27 Oct, 2024
+ */
+
+@Component
+public class RequestHeaderInterceptor implements WebGraphQlInterceptor {
+
+    @Override
+    public Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
+
+        String header1 = request.getHeaders().getFirst("header1");
+        String header2 = request.getHeaders().getFirst("header2");
+        String header3 = request.getHeaders().getFirst("header3");
+        String header4 = request.getHeaders().getFirst("header4");
+        String header5 = request.getHeaders().getFirst("header5");
+        String header6 = request.getHeaders().getFirst("header6");
+
+        request.configureExecutionInput(((executionInput, builder) ->
+                    builder.graphQLContext(
+                            Map.of(
+                                "header1", StringUtils.hasText(header1)? header1: "",
+                                "header2", StringUtils.hasText(header2)? header2: "",
+                                "header3", StringUtils.hasText(header3)? header3: "",
+                                "header4", StringUtils.hasText(header4)? header4: "",
+                                "header5", StringUtils.hasText(header5)? header5: "",
+                                "header6", StringUtils.hasText(header6)? header6: ""
+                            )
+                    ).build()
+                ));
+
+        return chain.next(request);
+    }
+}
